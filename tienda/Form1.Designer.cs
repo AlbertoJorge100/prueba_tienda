f14187a1abd00fee649237125a9acf7034f17639
Alberto <nolascojorge39@gmail.com>
2021-04-12 12:47:42 -0600

    primer commit

---
+++ tienda/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace tienda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbOpcion = new System.Windows.Forms.ComboBox();
+            this.lblProductos = new System.Windows.Forms.Label();
+            this.txbCantidad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbDireccion = new System.Windows.Forms.TextBox();
+            this.txbTelefono = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbOpcion
+            // 
+            this.cmbOpcion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOpcion.FormattingEnabled = true;
+            this.cmbOpcion.Items.AddRange(new object[] {
+            "Camisas $25",
+            "Vestidos $43",
+            "Pantalones de hombre $15",
+            "Pantalones de mujer   $15",
+            "Shorts $10",
+            "Pasarrillos $10"});
+            this.cmbOpcion.Location = new System.Drawing.Point(48, 265);
+            this.cmbOpcion.Name = "cmbOpcion";
+            this.cmbOpcion.Size = new System.Drawing.Size(282, 24);
+            this.cmbOpcion.TabIndex = 0;
+            this.cmbOpcion.SelectedIndexChanged += new System.EventHandler(this.cmbOpcion_SelectedIndexChanged);
+            // 
+            // lblProductos
+            // 
+            this.lblProductos.AutoSize = true;
+            this.lblProductos.Location = new System.Drawing.Point(483, 263);
+            this.lblProductos.Name = "lblProductos";
+            this.lblProductos.Size = new System.Drawing.Size(76, 17);
+            this.lblProductos.TabIndex = 1;
+            this.lblProductos.Text = "Productos:";
+            // 
+            // txbCantidad
+            // 
+            this.txbCantidad.Location = new System.Drawing.Point(349, 265);
+            this.txbCantidad.Name = "txbCantidad";
+            this.txbCantidad.Size = new System.Drawing.Size(86, 22);
+            this.txbCantidad.TabIndex = 2;
+            this.txbCantidad.Text = "1";
+            this.txbCantidad.TextChanged += new System.EventHandler(this.txbCantidad_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(48, 316);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 31);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.Location = new System.Drawing.Point(48, 365);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(75, 32);
+            this.btnPagar.TabIndex = 6;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = true;
+            this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(45, 426);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(72, 17);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(31, 52);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(157, 22);
+            this.txbNombre.TabIndex = 10;
+            this.txbNombre.Text = "x";
+            // 
+            // txbDireccion
+            // 
+            this.txbDireccion.Location = new System.Drawing.Point(31, 99);
+            this.txbDireccion.Name = "txbDireccion";
+            this.txbDireccion.Size = new System.Drawing.Size(157, 22);
+            this.txbDireccion.TabIndex = 11;
+            // 
+            // txbTelefono
+            // 
+            this.txbTelefono.Location = new System.Drawing.Point(31, 151);
+            this.txbTelefono.Name = "txbTelefono";
+            this.txbTelefono.Size = new System.Drawing.Size(157, 22);
+            this.txbTelefono.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Direccion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Telefono";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(204, 316);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(84, 31);
+            this.btnLimpiar.TabIndex = 16;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 677);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbTelefono);
+            this.Controls.Add(this.txbDireccion);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txbCantidad);
+            this.Controls.Add(this.lblProductos);
+            this.Controls.Add(this.cmbOpcion);
+            this.Name = "Form1";
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbOpcion;
+        private System.Windows.Forms.Label lblProductos;
+        private System.Windows.Forms.TextBox txbCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnPagar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox txbDireccion;
+        private System.Windows.Forms.TextBox txbTelefono;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ tienda/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace tienda
+{
+    public partial class Form1 : Form
+    {        
+        private int Cantidad;
+        private double SubTotal;
+        private double Total;
+        private double DescuentoTotal;
+        private String cadena;
+        private double[] precios = new double[6];
+        public Form1()
+        {
+            InitializeComponent();
+            this.Cantidad = 0;
+            this.SubTotal = 0.0;
+            this.Total = 0;
+            precios[0] = 25;
+            precios[1] = 43;
+            precios[2] = 15;
+            precios[3] = 15;
+            precios[4] = 10;
+            precios[5] = 10;
+
+            /*LLenar el combobox
+             * cmbOpcion.Items.Add("afasdf");
+            cmbOpcion.Items.Add("afasdf");
+            cmbOpcion.Items.Add("afasdf");
+            cmbOpcion.Items.Add("afasdf");
+            cmbOpcion.Items.Add("afasdf");*/
+        }
+
+        public int sumar(int parametro1, int parametro2)
+        {
+            int suma = parametro1 + parametro2;
+            return suma;
+        }
+
+        private void cmbOpcion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(cmbOpcion.SelectedItem.ToString());//
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {            
+
+            if (txbCantidad.Text.Equals(""))
+            {
+                //Si esta vacio retorna true
+                MessageBox.Show("Ingrese una cantidad porfavor");
+            }
+            else
+            {
+                //retorna false aqui tiene datos 
+                int indice = cmbOpcion.SelectedIndex;
+                double precio = precios[indice];
+                int cantidad = int.Parse(txbCantidad.Text);
+                //Subtotal es el precio total sin descuento
+                double subtotal = precio * cantidad;
+                double descuento = 0;
+                double descuentoMostrar = 0;
+                
+                //Validar los descuentos
+                if (cantidad <= 5)
+                {
+                    //la cantidad es menor a 5
+                    //Aplicamos el descuento respectivo de 10%
+                    descuentoMostrar = 0.10;
+                    //Descuento es el precio total con descuento
+                    double descontar = (subtotal * descuentoMostrar);
+                    //total con descuento
+                    descuento = (subtotal - descontar);
+                    
+                }
+                else if (cantidad > 5 && cantidad <= 10)
+                {
+                    //Aplicamos el descuento respectivo
+                    descuentoMostrar = 0.15;
+                    double descontar = (subtotal * descuentoMostrar);
+                    //total con descuento
+                    descuento = subtotal - descontar;
+                }
+                else if (cantidad > 10)
+                {
+                    //Aplicamos el descuento respectivo
+                    descuentoMostrar = 0.2;
+                    double descontar = (subtotal * descuentoMostrar);
+                    //total con descuento
+                    descuento = subtotal - descontar;
+                }
+                //Descuento total que se operar
+                this.DescuentoTotal = descuentoMostrar*cantidad;
+
+                //Almacenamos el total con descuento
+                this.Total = descuento;                
+                
+                lblProductos.Text = cmbOpcion.SelectedItem+ " cant: " + cantidad.ToString()
+                    + " total sin descuento: $" + subtotal + " total con descuento: $" + descuento;
+                this.Cantidad = Cantidad + cantidad;
+               
+            }           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int num1 = 20;
+            int num2 = 3;
+            int resultado=sumar(num1, num2);
+            //MessageBox.Show(resultado.ToString());
+        }
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            Boolean traspaso = ValidarCampos();
+            //validar si traspaso es verdadera o falsa, si es verdadero todos los campos tienen datos, si no no tienen
+            if (traspaso == true)
+            {
+                //trapaso es true
+                //Hay quemostrar los datos
+                //+= concatenacion, ir poniendo datos ala par de otro
+                this.cadena += "Cliente: " + txbNombre.Text + " Direccion: " + txbDireccion.Text + " Telefono: " + txbTelefono.Text
+                + " Descripcion: " + cmbOpcion.SelectedItem + " Cantidad: " + txbCantidad.Text + "\n Precio unitario: "
+                + precios[cmbOpcion.SelectedIndex].ToString()//precios requiere un indice para mostrar su valor
+                + " Descuento Total: " + this.DescuentoTotal
+                + " Total: " + this.Total + "\n";
+                lblResultado.Text = cadena;
+            }
+            else
+            {
+                MessageBox.Show("No deje ningun campo vacio","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txbNombre.Text = "";
+            txbDireccion.Text = "";
+            txbTelefono.Text = "";
+            txbCantidad.Text = "";
+            cmbOpcion.SelectedIndex = -1;
+            lblProductos.Text = "Productos: ";
+            this.SubTotal = 0;
+            this.DescuentoTotal = 0;
+            this.Total = 0;
+            this.Cantidad = 0;
+        }
+
+        private Boolean ValidarCampos()
+        {            
+            //Si los textbox son vacios nos retorna verdadero, pero si tienen un dato es false
+            //retornaran true si no tienen datos, pero si tienen retornan false
+            Boolean nombre = txbNombre.Text.Equals("");//true, false
+            Boolean direccion = txbDireccion.Text.Equals("");//true, false
+            Boolean telefono = txbTelefono.Text.Equals("");//true, false
+            Boolean cantidad = txbCantidad.Text.Equals("");//true, false
+            Boolean resultado;
+            
+            //Todas tienen que ser falsas para que puedan estar llenas de datos los textbox
+            if(nombre==false && direccion==false && telefono == false && cantidad==false)
+            {
+                //Textbox tienen datos
+                resultado = true;
+            }
+            else
+            {
+                //textbox no tienen datos
+                resultado = false;
+            }            
+            return resultado;            
+        }
+
+        private void txbCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
